552723b8410f4ca29ef16fa2c26825b1af2c7888
NagarajuRavula <nagaraju.ravula@imaginea.com>
2017-10-16 10:57:22 +0530

    after reading patient data from file

--- server.js
+++ server.js
@@ -1,27 +1,50 @@
 
 var express = require('express');
 var request = require('request');
-
+var fs = require('fs');
 var app = express();
 
 app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res) {
-    request('http://localhost:8080/UserManagement/rest/UserService/patients/', function(error, response, body) {
-        if(error)
-        {
-            res.send(error)
-        }
-        else
-        {
-            res.render('pages/patient', {
-             patients: body
-          });
+  //  request('resources/patient.txt', function(error, response, body) {
+    //    if(error)
+      //  {
+        //    res.send(error)
+        //}
+        //else
+        //{
+          //  res.render('pages/patient', {
+            // patients: body
+          //});
 
-        }
-    });
+        //}
+    //});
+
+  
+fs.readFile('resources/patient.json', 'utf8', function (err, data) {
+  if (err) 
+    {
+        res.send(err);
+    }
+    else
+    {
+       res.render('pages/patient', {
+         patients: data
+      });     
+    }
 });
+
+
+});
+
+
+
+
+
+
+
 
 app.listen(8066);
 console.log('8066 server port running');
